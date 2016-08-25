b5fe09ecab0ef747c1c6ca30b977cece306737bb
tony24681379 <yellow24681379@gmail.com>
2016-08-25 17:53:25 +0800

    fix change password bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,14 +41,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up)  do |user_params|
-      user_params.permit( :account, :username, :email, :password)
+      user_params.permit( :account, :username, :email, :password, :password_confirmation, :current_password)
     end
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update)  do |user_params|
-      user_params.permit( :account, :username, :email, :password)
+      user_params.permit( :account, :username, :email, :password, :password_confirmation, :current_password)
     end
   end
 
