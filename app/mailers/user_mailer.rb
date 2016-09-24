968cd61ca9a21dd4d4b888d0cde3959b2cd9ec6b
ChunChenLin <tp632333846@gmail.com>
2016-09-24 07:54:38 +0000

    fill mail's content with daily problems

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,20 @@
 class UserMailer < ApplicationMailer
-  default :from => "XXX@gmail.com"
+  #default :from => "tp632333846@gmail.com"
+  default from: "nthulibrails@gmail.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.notify_problem.subject
   #
+  #def welcome_email
+  #  @user = User.first
+  #  mail(:to => @user.email, :subject => 'Welcome to use the system')
+  #end
   def notify_problem
-    @greeting = "Hi"
-    mail(:to => "XXX@yahoo.com.tw", :subject => "New Comment")
+    @receiver = "tp6_3846@yahoo.com.tw"
+    @another = "smluo@lib.nthu.edu.tw"
+    @daily_problems = DailyProblem.where("created_at > ?", Time.now.beginning_of_day)
+    mail(:to => @receiver, :cc => @another, :subject => "測試")
     #mail to: "to@example.org"
   end
 end
