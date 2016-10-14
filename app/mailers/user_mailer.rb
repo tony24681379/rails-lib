29ef04b2589024d827cb621f172d9513670a1bce
ChunChenLin <tp632333846@gmail.com>
2016-10-14 16:40:39 +0800

    send to multiple receivers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,10 +10,8 @@ class UserMailer < ApplicationMailer
   #  @user = User.first
   #  mail(:to => @user.email, :subject => 'Welcome to use the system')
   #end
-  def notify_problem
-    @receiver = "tp6_3846@yahoo.com.tw"
-    @another = "smluo@lib.nthu.edu.tw"
-    #@another2 = "lovemeimeicat@gmail.com"
+  def self.notify_problem
+    
     @daily_problems = DailyProblem.where("date >= ?", Time.now.beginning_of_day)
     
     if @daily_problems.count == 0 
@@ -21,29 +19,46 @@ class UserMailer < ApplicationMailer
     end
     
     @daily_problems.each do |daily_problem|
+      
       if not daily_problem.comment.blank?
         daily_problem.situation = "已完成"
+        next
       end
-      group = Machine.where("machine_id = ?", daily_problem.machine_id).first.maintain_group
-      case group
-      when 'Kiosk查詢平台'
-        daily_problem.maintainer_id = '陳玉芬'
-        #@another += ", yfchen@lib.nthu.edu.tw"
-      when '非靠卡電腦'
-        daily_problem.maintainer_id = '高見成'
-        #@another += ", cckao@lib.nthu.edu.tw" 
-      when '一般靠卡電腦'
-        daily_problem.maintainer_id = '高見成'
-        #@another += ", cckao@lib.nthu.edu.tw"
-      when '預約靠卡電腦'
-        daily_problem.maintainer_id = '高見成'
-        #@another += ", cckao@lib.nthu.edu.tw"
-      when '多媒體設備'
-        daily_problem.maintainer_id = '黃美惠'
-        #@another += ", mhhuang@lib.nthu.edu.tw"
+      
+      if daily_problem.situation == "待處理"
+        send_mail(daily_problem).deliver_now!
       end
       
     end
+    # :cc => @another
+    #mail(:to => @receiver, :subject => "巡檢系統通知測試")
+  end
+  
+  def send_mail(daily_problem)
+    @receiver = "tp6_3846@yahoo.com.tw"
+    @another = "smluo@lib.nthu.edu.tw"
+    
+    group = Machine.where("machine_id = ?", daily_problem.machine_id).first.maintain_group
+    case group
+    when 'Kiosk查詢平台'
+      daily_problem.maintainer_id = '陳玉芬'
+      @another += ", yfchen@lib.nthu.edu.tw"
+    when '非靠卡電腦'
+      daily_problem.maintainer_id = '高見成'
+      @another += ", cckao@lib.nthu.edu.tw" 
+    when '一般靠卡電腦'
+      daily_problem.maintainer_id = '高見成'
+      @another += ", cckao@lib.nthu.edu.tw"
+    when '預約靠卡電腦'
+      daily_problem.maintainer_id = '高見成'
+      @another += ", cckao@lib.nthu.edu.tw"
+    when '多媒體設備'
+      daily_problem.maintainer_id = '黃美惠'
+      @another += ", mhhuang@lib.nthu.edu.tw"
+    end
+    
+    @daily_problem = daily_problem
     mail(:to => @receiver, :cc => @another, :subject => "巡檢系統通知測試")
   end
+  
 end
